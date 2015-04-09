186c95ca30ca005288bd84612cfccf74de330283
Andre Lamelas <andre@lamelas.org>
2015-04-09 15:53:32 +0100

    Update for the last version of the Twitter gem. Removed the request limit verification.

--- unfollowall.rb
+++ unfollowall.rb
@@ -4,54 +4,43 @@ require 'Twitter'
 backup_filename = "backup.txt"
 
 begin
-	Twitter.configure do |config|
-	  config.consumer_key = "" # place your consumer key 
-	  config.consumer_secret = "" # place your consumer secret 
-	  config.oauth_token = "" # place oauth token 
-	  config.oauth_token_secret = "" # place oauth token secret
-	end
+    client = Twitter::REST::Client.new do |config|
+        config.consumer_key = "" # place your consumer key 
+        config.consumer_secret = "" # place your consumer secret 
+        config.access_token = "" # place oauth token 
+        config.access_token_secret = "" # place oauth token secret
+    end
 
-	puts "Backing up the users you follow. Just in case..."
-	File.open(backup_filename, "a") do |file|
-		Twitter.friend_ids.each do |id|
-			file.write("#{id}\n")
-		end
-	end
-	
+    friend_ids = client.friend_ids
 
-	puts "The IDs for all the users you follow where backed up to the file #{backup_filename}"
+    puts "Backing up the users you follow. Just in case..."
+    File.open(backup_filename, "a") do |file|
+        friend_ids.each do |id|
+            file.write("#{id}\n")
+        end
+    end
 
-	following = Twitter.friend_ids.all.length
-	puts "You're following #{following} users"
+    puts "The IDs for all the users you follow where backed up to the file #{backup_filename}"
 
-	rate_limit_status = Twitter.rate_limit_status
-	puts "#{rate_limit_status.remaining_hits} Twitter API request(s) remaining for the next #{((rate_limit_status.reset_time - Time.now) / 60).floor} minutes and #{((rate_limit_status.reset_time - Time.now) % 60).round} seconds"
-	puts "When the API limit is reached, this script just quits"
+    following = friend_ids.to_h[:ids].length
+    puts "You're following #{following} users"
 
-	puts "Starting le grand Twitter unfollowing"
-	Twitter.friend_ids.each do |id|
-		begin
-			
-			if rate_limit_status.remaining_hits == 0
-				puts "Reached API request limit. Going to sleep for #{((rate_limit_status.reset_time - Time.now) / 60).floor + 1.0}. You can stop this script at any time and resume later."
-				sleep(((rate_limit_status.reset_time - Time.now) / 60).floor + 1.0) 
-			end
+    puts "Starting le grand Twitter unfollowing"
+    friend_ids.each do |id|
+        begin
+            unfollowed_user = client.unfollow(id)
+            following -= 1
+            puts "Just unfollowed #{unfollowed_user.first.screen_name} (#{unfollowed_user.first.name}). #{following} left." if following > 0
+            STDOUT.flush
+            sleep(1.5)
+        rescue Exception => e
+            puts "An error ocurred when trying to unfollow user with ID #{id}"
+        end
+    end
 
-			unfollowed_user = Twitter.unfollow(id)
-			following -= 1
-			puts "Just unfollowed #{unfollowed_user.first.screen_name} (#{unfollowed_user.first.name}). #{following} left." if following > 0
-			STDOUT.flush
-			sleep(1.5)
-		rescue Exception => e
-			puts "An error ocurred when trying to unfollow user with ID #{id}"
-		end
-	end
-
-	if following == 0
-		puts "FREE AT LAST!"
-	else
-		puts "You're still following #{following} users. The API will reset in #{((rate_limit_status.reset_time - Time.now) / 60).floor} minutes and #{((rate_limit_status.reset_time - Time.now) % 60).round} seconds"
-	end
+    if following == 0
+        puts "FREE AT LAST!"
+    end
 
 rescue => e
   p e.message
